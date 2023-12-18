979f297f50c1e57d6d2cd4571283b80cb51f41ec
temeirro <temeirro@DESKTOP-P6FAQH9>
2023-12-18 22:45:57 +0200

    add page remake

--- src/components/Category/AddCategory.tsx
+++ src/components/Category/AddCategory.tsx
@@ -1,56 +1,112 @@
-import React, { useState } from "react";
+import {Button, Divider, Form, Input, message, Upload, UploadFile, UploadProps} from "antd";
+import {useState} from "react";
+import {RcFile, UploadChangeParam} from "antd/es/upload";
+import {LoadingOutlined, PlusOutlined} from '@ant-design/icons';
+import {ICategoryCreate} from "../types.ts";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { Input, Button, Row, Col } from 'antd';
+import {useNavigate} from "react-router-dom";
 
-const AddCategory: React.FC = () => {
-  const navigate = useNavigate();
-  const [categoryName, setCategoryName] = useState("");
-  const [categoryImage, setCategoryImage] = useState("");
+const CategoryCreatePage = () => {
 
-  const handleAddCategory = () => {
-    const formData = new FormData();
-    formData.append("name", categoryName);
-    formData.append("image", categoryImage);
+    const [file, setFile] = useState<File | null>(null);
+    const navigate = useNavigate();
+    const onSubmit = async (values: any) => {
+        if(file==null) {
+            message.error("Оберіть фото!");
+            return;
+        }
+        const model : ICategoryCreate = {
+            name: values.name,
+            image: file
+        };
+        try {
+            await axios.post("http://pv116.rozetka.com/api/categories", model,{
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+            navigate("/");
+        }
+        catch (ex) {
+            message.error('Помилка створення категорії!');
+        }
+    }
+    const onSubmitFailed = (errorInfo: any) => {
+        console.log("Error Form data", errorInfo);
+    }
 
-    axios.post("http://pv116.rozetka.com/api/categories", formData)
-        .then((response) => {
-          setCategoryName("");
-          setCategoryImage("");
-          navigate('/');
-        })
-        .catch((error) => {
-          console.error("Error adding category:", error);
-        });
-  };
+    type FieldType = {
+        name?: string;
+    };
 
-  const handleCancel = () => {
-    navigate('/');
-  }
+    const beforeUpload = (file: RcFile) => {
+        const isImage = /^image\/\w+/.test(file.type);
+        if (!isImage) {
+            message.error('Оберіть файл зображення!');
+        }
+        const isLt2M = file.size / 1024 / 1024 < 10;
+        if (!isLt2M) {
+            message.error('Розмір файлу не повинен перевищувать 10MB!');
+        }
+        console.log("is select", isImage && isLt2M);
+        return isImage && isLt2M;
+    };
+    const handleChange: UploadProps['onChange'] = (info: UploadChangeParam<UploadFile>) => {
+        const file = info.file.originFileObj as File;
+        setFile(file);
+    };
 
-  return (
-    <Row justify="center"  style={{ height: '100vh' }}>
-      <Col span={8} style={{ textAlign: 'center', padding: '24px', borderRadius: '8px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)' }}>
-        <h2 style={{ marginBottom: '16px' }}>Add Category</h2>
-        <Input
-          type="text"
-          placeholder="Category name"
-          value={categoryName}
-          onChange={(e) => setCategoryName(e.target.value)}
-        />
-        <Input  style={{ marginTop: '10px' }}
-            type="file"
-            placeholder="Category image"
-            onChange={(e) => setCategoryImage(e.target.files[0])}
-        />
+    const uploadButton = (
+        <div>
+            <PlusOutlined/>
+            <div style={{marginTop: 8}}>Upload</div>
+        </div>
+    );
 
-        <Row justify="space-between" style={{ marginTop: '16px' }}>
-          <Button type="primary" onClick={handleAddCategory}>Add Category</Button>
-          <Button onClick={handleCancel}>Cancel</Button>
-        </Row>
-      </Col>
-    </Row>
-  );
-};
+    return (
+        <>
+            <Divider>Додати категорію</Divider>
+            <Form
+                labelCol={{span: 8}}
+                wrapperCol={{span: 16}}
+                style={{maxWidth: 600}}
+                initialValues={{remember: true}}
+                onFinish={onSubmit}
+                onFinishFailed={onSubmitFailed}>
 
-export default AddCategory;
+                <Form.Item<FieldType>
+                    label="Назва"
+                    name="name"
+                    rules={[{required: true, message: 'Вкажіть назву!'}]}
+                >
+                    <Input/>
+                </Form.Item>
+                <div>
+                    <Upload
+                        name="avatar"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        action="#"
+                        beforeUpload={beforeUpload}
+                        onChange={handleChange}
+                        accept={"image/*"}
+                    >
+                        {file ?
+                            <img src={URL.createObjectURL(file)} alt="avatar" style={{width: '100%'}}/> : uploadButton}
+                    </Upload>
+                </div>
+
+
+                <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                    <Button type="primary" htmlType="submit">
+                        Додати
+                    </Button>
+                </Form.Item>
+
+            </Form>
+        </>
+    )
+}
+
+export default CategoryCreatePage;
